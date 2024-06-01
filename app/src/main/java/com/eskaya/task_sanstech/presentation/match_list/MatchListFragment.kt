667b809ac3b3e82e0a40d7fc414ec6b264153153
Esra Kaya <esra.kaya@bil.omu.edu.tr>
2024-06-02 00:13:49 +0300

    Update UI text and add progress container.

--- app/src/main/java/com/eskaya/task_sanstech/presentation/match_list/MatchListFragment.kt
+++ app/src/main/java/com/eskaya/task_sanstech/presentation/match_list/MatchListFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -72,6 +73,7 @@ class MatchListFragment : Fragment(), MatchAdapterListener {
     }
 
     private fun handleLoading(loading: Boolean) {
+        binding.containerProgress.isVisible = loading
     }
 
     private fun handleError(error: Any) {
